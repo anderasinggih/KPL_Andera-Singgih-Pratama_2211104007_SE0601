d0755a3ae3f518fefbd2b5e7a7ea4bcf58968031
Andera Singgih Pratama <anderasinggih1@gmail.com>
2025-03-13 15:48:47 +0700

    menambahkan class haloGeneric dengan method SapaUser

--- 05_Generics/tp/Program.cs
+++ 05_Generics/tp/Program.cs
@@ -1,23 +1,10 @@
-using System;
+﻿using System;
 
-public class HaloGeneric
+class HaloGeneric
 {
-    public static void SapaUser<T>(T user)
+    public void Sapauser<T>(T user)
     {
-        Console.WriteLine($"Halo Bray {user}");
-    }
-}
-
-public class DataGeneric<T>
-{
-    private T data;
-    public DataGeneric(T data)
-    {
-        this.data = data;
-    }
-    public void PrintData()
-    {
-        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+        Console.WriteLine($"Halo Bro {user}");
     }
 }
 
@@ -25,8 +12,7 @@ class Program
 {
     static void Main()
     {
-        HaloGeneric.SapaUser("Mohammad Dhimas Afrizal");
-        DataGeneric<string> data = new DataGeneric<string>("2211104023");
-        data.PrintData();
+        HaloGeneric halo = new HaloGeneric();
+        halo.Sapauser("Andera SInggih Pratama");
     }
 }
