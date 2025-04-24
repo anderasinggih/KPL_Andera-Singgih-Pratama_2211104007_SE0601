64a01ab3f2ab87a746356157a5946b85133bd257
Andera Singgih Pratama <anderasinggih1@gmail.com>
2025-04-24 14:41:30 +0700

    menambahkan class haloGeneric dengan method SapaUser

--- 05_Generics/jurnal/Program.cs
+++ 05_Generics/jurnal/Program.cs
@@ -1,13 +1,23 @@
 using System;
 
-class Penjumlahan
+public class HaloGeneric
 {
-    public static T JumlahTigaAngka<T>(T angka1, T angka2, T angka3) where T : struct
+    public static void SapaUser<T>(T user)
     {
-        dynamic a = angka1;
-        dynamic b = angka2;
-        dynamic c = angka3;
-        return (T)(a + b + c);
+        Console.WriteLine($"Halo Bro {user}");
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
     }
 }
 
@@ -15,14 +25,8 @@ class Program
 {
     static void Main()
     {
-        // Sesuai dengan NIM 2211104026, gunakan tipe data int
-        int angka1 = 22;
-        int angka2 = 11;
-        int angka3 = 10;
-
-        int hasil = Penjumlahan.JumlahTigaAngka(angka1, angka2, angka3);
-        Console.WriteLine("NIM saya 2211104023");
-        Console.WriteLine("Penjumlahan tiga input angka dari 2-digit NIM saya");
-        Console.WriteLine($"Hasil penjumlahan: {hasil}");
+        HaloGeneric.SapaUser("Aderas Singgih");
+        DataGeneric<string> data = new DataGeneric<string>("2211104007");
+        data.PrintData();
     }
 }
