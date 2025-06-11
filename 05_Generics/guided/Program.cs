d67adaa57d285cbf609f37324d54b49ba6b7693f
Andera Singgih Pratama <anderasinggih1@gmail.com>
2025-06-11 19:07:29 +0700

    MENAMBAHKAN CLASS IMPLE DATE BASE

--- 05_Generics/guided/Program.cs
+++ 05_Generics/guided/Program.cs
@@ -1,38 +1,79 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace modul5
+<<<<<<< HEAD
+public class Penjumlahan
 {
-    public class Program
+    // Method generic untuk menjumlahkan tiga angka
+    public static T JumlahTigaAngka<T>(T angka1, T angka2, T angka3) where T : struct
     {
-        public static void Main()
+        dynamic a = angka1;
+        dynamic b = angka2;
+        dynamic c = angka3;
+        return a + b + c;
+=======
+class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    // Konstruktor
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    // Method untuk menambahkan data baru
+    public void AddNewData(T item)
+    {
+        storedData.Add(item);
+        inputDates.Add(DateTime.UtcNow);
+    }
+
+    // Method untuk mencetak semua data
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
         {
-            int[] arr = { 0, 1, 2, 3, 4 };
-            List<int> list = new List<int>();
-            for (int x = 5; x < 10; x++)
-            {
-                list.Add(x);
-            }
-            ProcessItems<int>(arr);
-            ProcessItems<int>(list);
+            Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, yang disimpan pada waktu UTC: {inputDates[i]}");
         }
-        static void ProcessItems<T>(IList<T> coll)
-        {
-            // IsReadOnly returns True for the array and False for the List.
-            System.Console.WriteLine
-            ("IsReadOnly returns {0} for this collection.",
-            coll.IsReadOnly);
-            // The following statement causes a run-time exception for the
-            // array, but not for the List.
-            //coll.RemoveAt(4);
-            foreach (T item in coll)
-            {
-                System.Console.Write(item.ToString() + " ");
-            }
-            System.Console.WriteLine();
-        }
+>>>>>>> implementasi-generic-class
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+<<<<<<< HEAD
+        // NIM 2211104007 -> Berakhiran 2, maka tipe data adalah float
+        string nim = "2211104007";
+        Console.WriteLine($"NIM saya yaitu {nim}");
+
+        // Mengambil 2-digit angka dari NIM
+        float angka1 = 22.0f;
+        float angka2 = 11.0f;
+        float angka3 = 10.0f;
+
+        // Memanggil method generic dengan tipe data float
+        float hasil = Penjumlahan.JumlahTigaAngka(angka1, angka2, angka3);
+
+        // Menampilkan hasil sesuai format
+        Console.WriteLine($"Hasil penjumlahan: {hasil}");
+=======
+        Console.WriteLine("NIM saya yaitu 2211104007");
+
+        // Membuat instance SimpleDataBase untuk tipe data float
+        SimpleDataBase<float> database = new SimpleDataBase<float>();
+
+        // Menambahkan tiga data dari dua digit NIM (22, 11, 10)
+        database.AddNewData(22f);
+        database.AddNewData(11f);
+        database.AddNewData(10f);
+
+        // Menampilkan semua data yang tersimpan
+        database.PrintAllData();
+>>>>>>> implementasi-generic-class
     }
 }
