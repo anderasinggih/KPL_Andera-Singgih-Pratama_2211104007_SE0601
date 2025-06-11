9554ed7387501ebbc0ceb8e6a006b859c90b0df1
Andera Singgih Pratama <anderasinggih1@gmail.com>
2025-06-11 19:02:45 +0700

    update

--- 05_Generics/jurnal/Program.cs
+++ 05_Generics/jurnal/Program.cs
@@ -15,13 +15,13 @@ class Program
 {
     static void Main()
     {
-        // Sesuai dengan NIM 2211104026, gunakan tipe data int
+        // Sesuai dengan NIM 2211104007, gunakan tipe data int
         int angka1 = 22;
         int angka2 = 11;
         int angka3 = 10;
 
         int hasil = Penjumlahan.JumlahTigaAngka(angka1, angka2, angka3);
-        Console.WriteLine("NIM saya 2211104023");
+        Console.WriteLine("NIM saya 2211104007");
         Console.WriteLine("Penjumlahan tiga input angka dari 2-digit NIM saya");
         Console.WriteLine($"Hasil penjumlahan: {hasil}");
     }
