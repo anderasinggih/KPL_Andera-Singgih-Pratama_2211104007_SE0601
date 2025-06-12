35b3734e7daa18be86ec717ed381f878cb6a2610
Andera Singgih Pratama <anderasinggih1@gmail.com>
2025-06-12 21:16:22 +0700

    p

--- 15_Jurnal/modul15_2211104007/Form2.Designer.cs
+++ 15_Jurnal/modul15_2211104007/Form2.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace modul15_2211104007
+﻿namespace modul15_2211104002
 {
-    partial class Form2 : System.Windows.Forms.Form
+    partial class Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -41,5 +41,65 @@
             this.btnRegisterUser.Name = "btnRegisterUser";
             this.btnRegisterUser.Size = new System.Drawing.Size(75, 23);
             this.btnRegisterUser.TabIndex = 9;
+            this.btnRegisterUser.Text = "login";
+            this.btnRegisterUser.UseVisualStyleBackColor = true;
+            // 
+            // txtUsernameRegister
+            // 
+            this.txtUsernameRegister.Location = new System.Drawing.Point(259, 178);
+            this.txtUsernameRegister.Name = "txtUsernameRegister";
+            this.txtUsernameRegister.Size = new System.Drawing.Size(100, 22);
+            this.txtUsernameRegister.TabIndex = 8;
+            // 
+            // txtPasswordRegister
+            // 
+            this.txtPasswordRegister.Location = new System.Drawing.Point(441, 178);
+            this.txtPasswordRegister.Name = "txtPasswordRegister";
+            this.txtPasswordRegister.Size = new System.Drawing.Size(100, 22);
+            this.txtPasswordRegister.TabIndex = 7;
+            this.txtPasswordRegister.UseSystemPasswordChar = true;
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(456, 150);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(67, 16);
+            this.Password.TabIndex = 6;
+            this.Password.Text = "Password";
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Location = new System.Drawing.Point(271, 150);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(70, 16);
+            this.Username.TabIndex = 5;
+            this.Username.Text = "Username";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRegisterUser);
+            this.Controls.Add(this.txtUsernameRegister);
+            this.Controls.Add(this.txtPasswordRegister);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRegisterUser;
+        private System.Windows.Forms.TextBox txtUsernameRegister;
+        private System.Windows.Forms.TextBox txtPasswordRegister;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Label Username;
+    }
+}
